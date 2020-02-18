e205d33a74633bb9e19acb22b2adbfda5214e53d
Robert Link <robert.link@pnnl.gov>
2020-02-18 16:39:14 -0500

    Add readpkl function to read python pickle files.

--- R/readnpy.R
+++ R/readnpy.R
@@ -8,3 +8,24 @@ readNPY <- function(filename)
     np <- reticulate::import('numpy')
     np$load(filename)
 }
+
+#' Read a pickled python object from a file
+#'
+#' Read data saved with the python \code{pickle} module.
+#'
+#' Python lists will return come through as lists; numpy arrays will come through as R
+#' arrays (or matrices in 2-d).  I haven't tested what dictionaries turn into, but I'd
+#' guess something like a named list.  If you try it sometime, let me know.
+#'
+#' @param filename Name of the file to read
+#' @return An R object that more or less works like the python object; see details.
+#' @export
+readpkl <- function(filename)
+{
+    bi <- reticulate::import('builtins')
+    pkl <- reticulate::import('pickle')
+    fh <- bi$open(filename,'rb')
+    rtn <- pkl$load(fh)
+    fh$close()
+    rtn
+}
--- tests/testthat/test_readNPY.R
+++ tests/testthat/test_readNPY.R
@@ -6,3 +6,13 @@ test_that('Numpy file is read correctly.', {
     m2 <- matrix(1:9, nrow=3, byrow = TRUE)
     expect_equal(m1, m2)
 })
+
+test_that('Pickle file is read correctly.', {
+    filename <- 'data/testdata.pkl'
+    l1 <- readpkl(filename)
+    expect_is(l1, 'list')
+    expect_true(all(sapply(l1, is.matrix)))
+    l2 <- list(matrix(0, nrow=2, ncol=2),
+            matrix(1, nrow=2, ncol=3))
+    expect_equal(l1, l2)
+})
